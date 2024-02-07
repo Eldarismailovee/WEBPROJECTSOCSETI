6f10847d5517074e5dcb8eb5e7a3f42ce7a4c219
Eldarismailovee <158322946+Eldarismailovee@users.noreply.github.com>
2024-02-07 20:56:07 +0200

    Add files via upload

---
+++ Pages/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Pages.ViewModel
+{ 
+    public class  RegisterViewModel
+    {
+        public class RegisterModel
+        {
+        }
+    }
+}
